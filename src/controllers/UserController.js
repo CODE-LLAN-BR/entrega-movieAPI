c8ba1622e7bafc4e9afa99a70c7d369cd6f1c3fc
Leonardo Nunes <leonardonunes1314@gmail.com>
2024-11-28 13:04:50 -0300

    config UserController,password encryption

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,15 +1,34 @@
+//error handling
 const AppError = require("../utils/AppError");
+
+//connection to data bank
+const sqliteConnection = require("../database/sqlite");
+
+//password encryption
+const {hash} = require('bcryptjs');
 
 class UsersController {
 
- create (request, response) {
+  async create (request, response) {
     const { name, email, password } = request.body;
 
-    if(!name){
-      throw new AppError("Nome é obrigatório!")
+    const database = await sqliteConnection();
+
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+    if(checkUserExists) {
+      throw new AppError("Este email já está em uso.");
     }
 
-    response.json({ name, email, password });
+    const hashPassword = await hash(password,8);
+
+
+    await database.run("INSERT INTO users (name ,email ,password) VALUES(? ,? ,?)",[name ,email ,hashPassword]);
+
+
+
+    return response.status(201).json
+
  }
 
 }
