618842e6fb6c1ef297c6d3af95c11038ace97ebd
Leonardo Nunes <leonardonunes1314@gmail.com>
2024-12-04 12:26:54 -0300

    knex migrations config

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,15 @@
-const {path} = require('path');
+const path = require('path');
 
 module.exports = {
 
   development: {
     client: 'sqlite3',
     connection: {
-      filename: path.resolve(__dirname ,"src" ,"database","database.db")
+      filename: path.resolve(__dirname,"src","database","database.db")
+    },
+    migrations: {
+      directory:path.resolve(__dirname,"src","database","knex","migrations")
     },
     useNullAsDefault: true
-  },
+  }
 };
---
+++ src/database/knex/migrations/20241204144209_QB_movie_notes.js
@@ -0,0 +1,18 @@
+
+
+exports.up = knex => knex.schema.createTable("movie_notes", table =>{
+    
+    table.increments("id");
+    table.text("movie_title");
+    table.text("about");
+    table.integer("user_review");
+    table.integer("user_id").references("id").inTable("users");
+
+    table.timestamp("created_at").default(knex.fn.now())
+    table.timestamp("updated_at").default(knex.fn.now())
+
+});
+
+
+exports.down = knex => knex.schema.dropTable("movie_notes");
+
